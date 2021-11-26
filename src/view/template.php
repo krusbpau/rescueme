5230be94202c998827257ee82eab2bd663a8db02
Pauliina Krusberg <pauliina.krusberg@edu.sasky.fi>
2021-11-26 18:14:19 +0200

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -7,7 +7,17 @@
   </head>
   <body>
    <header>
-    <h1><a href="<?=BASEURL?>">Rescue me</a> </h1>
+    <h1><a href="<?=BASEURL?>">Rescue me</a></h1>
+    <div class="profile">
+     <?php
+       if (isset($_SESSION['user'])) {
+        echo "<div>$_SESSION[user]</div>";
+        echo "<div><a href='logout'->Kirjaudu ulos</a></div>";
+      } else {
+        echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+      }
+     ?>
+   </div>
    </header>
    <section>
     <?=$this->section('content')?>
